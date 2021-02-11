eed4c8e81649a9614eac1b387661f99473a2aa36
ipatelanand@gmail.com <ipatelanand@gmail.com>
2021-02-11 10:00:21 -0500

    testing investment score

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -6,7 +6,7 @@ class App extends React.Component {
 	createListing = (event) => {
 		event.preventDefault()
 
-		let score = this.newPrice
+		let score = newPrice
 		let index =
 			(this.newOccupancyTraditionalPercentPerYear *
 				(12 * this.newRentalValueTraditional -
