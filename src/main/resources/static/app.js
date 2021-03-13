5ab1ef25e3a3929772a4e897f725c391d59643e4
ipatelanand@gmail.com <ipatelanand@gmail.com>
2021-03-13 12:32:12 -0500

    is react up?

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,7 +1,3 @@
-import { Auth0Provider } from "@auth0/auth0-react"
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-
 class App extends React.Component {
 	state = {
 		listing: [],
